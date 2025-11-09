efd76f58b7be1d1d60c3436b55b95eefc96826ab
madexxx <valera_cool@inbox.ru>
2025-11-09 20:31:53 +0500

    Готовый калькулятор ИМТ

--- main.go
+++ main.go
@@ -8,21 +8,85 @@ package main
 // 35-40 - ожирение второй степени
 // > 40 - ожирение третьей степени (морбидное)
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math"
+	"strings"
+)
 
-func 
+func getUserInput() (float64, float64) {
+	var height, weight float64
+	fmt.Print("Укажите свой рост в см: ")
+	fmt.Scan(&height)
+	fmt.Print("Укажите свой вес в кг: ")
+	fmt.Scan(&weight)
+	return weight, height
+}
+
+func calc(height, weight float64) (float64, error) {
+	height = height / 100
+	if height <= 0 || weight <= 0 {
+		return 0, errors.New("error: wrong user parapetres")
+	}
+	BMI := weight / math.Pow(height, 2)
+	return BMI, nil
+
+}
+
+func diagType(result float64) {
+
+	diagnosis := map[int]string{
+		1: "сильный дефицит массы тела",
+		2: "дефицит массы тела",
+		3: "норма",
+		4: "избыточная масса тела (предожирение)",
+		5: "ожирение первой степени",
+		6: "ожирение второй степени",
+		7: "ожирение третьей степени (морбидное)",
+	}
+
+	switch {
+	case result < 16:
+		fmt.Printf("Ваш индекс массы тела: %.0f\nВаш диагноз: %s\n", result, diagnosis[1])
+	case result < 18.5:
+		fmt.Printf("Ваш индекс массы тела: %.0f\nВаш диагноз: %s\n", result, diagnosis[2])
+	case result < 25:
+		fmt.Printf("Ваш индекс массы тела: %.0f\nВаш диагноз: %s\n", result, diagnosis[3])
+	case result < 30:
+		fmt.Printf("Ваш индекс массы тела: %.0f\nВаш диагноз: %s\n", result, diagnosis[4])
+	case result < 35:
+		fmt.Printf("Ваш индекс массы тела: %.0f\nВаш диагноз: %s\n", result, diagnosis[5])
+	case result < 40:
+		fmt.Printf("Ваш индекс массы тела: %.0f\nВаш диагноз: %s\n", result, diagnosis[6])
+	default:
+		fmt.Printf("Ваш индекс массы тела: %.0f\nВаш диагноз: %s\n", result, diagnosis[7])
+
+	}
+
+}
 
 func main() {
 
-	var height float64
-	var weight float64
+	for {
+		fmt.Println("\nНачинаем расчет индекса массы тела.")
+		var choice string
+		weight, height := getUserInput()
+		result, err := calc(height, weight)
+		if err != nil {
+			fmt.Println("\nОшибка: введены некоректные параметры рост/вес.")
+			continue
+		}
+		diagType(result)
 
-	fmt.Println("Укажите свой рост в см: ")
-	fmt.Scan(&height)
-
-	fmt.Println("Укажите свой рост в см: ")
-	fmt.Scan(&weight)
-
-	fmt.Printf("Ваш индекс массы тела равен %.f")
+		fmt.Print("Хотите продолжить? (y/n): ")
+		fmt.Scanln(&choice)
+		fmt.Scanln(strings.ToLower(choice))
+		if choice != "y" {
+			break
+		} else {
+			continue
+		}
+	}
 
 }
